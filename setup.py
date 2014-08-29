e57b7d14bfede67c1ef0ad2e43b8361b70951732
Snifer <pesaurio@gmail.com>
2014-08-29 18:35:23 -0400

    Verificacion de Nmap solucionado

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ import subprocess
 def main():
     import subprocess
     import os
+
     try:
         subprocess.call(["nmap", "--version"])
-    except OSError as error:
-        if error.errno = os.errno.ENOENT:
+    except ImportError:
         print 'Nmap no se tiene instalado... O se encuentra en otro directorio. Porfavor instalar, o especificar el directorio en ~/gpt.conf'
-        os.exit()
-
+        os.exit(1)
 
 if __name__ == "__main__":
     main()
+
+
+
+
