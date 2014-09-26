b2d986d4f91827ebc694d3ad9503c68c8d28be92
Snifer <pesaurio@gmail.com>
2014-09-26 10:55:27 -0400

    Modify

--- ip.py
+++ ip.py
@@ -1,43 +1,19 @@
 import socket
 
-
-
 class GetIp:
 
-  
-
   def __init__(self):
-
-    
-
     self.ip = ""
 
-    
-
   def return_ip(self, hostname):
-
-      
-
       try:
-
-  
-
           self.ip = socket.gethostbyname(hostname)
-
           print self.ip
-
-  
 
       except:
 
-  
-
           print "Verify your connection please :-(!"
 
-
-
-target = raw_input("Target: http://")
-
+target = raw_input("Insert site for obtain IP: http://")
 ip = GetIp()
-
 ip.return_ip(target)
