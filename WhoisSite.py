68882b1bdfb5f8c20a77a9b069c10c8334c44741
Jose Moruno Cadima <pesaurio@gmail.com>
2015-11-28 22:27:41 -0400

    Change retrieve information

--- WhoisSite.py
+++ WhoisSite.py
@@ -1,17 +1,13 @@
 import whois
 
-data = raw_input("Insert URL for the <Whois> site information: ")
-whoisData = whois.whois(data)
-whoisSite = data
+data = raw_input("Insert URL for the <Whois> site information: www.")
+domain = whois.query(data)
 
 print '\n'
-print '********************************************'
-print '*  Whois information'+ ' ' + str(whoisSite)+ '*'
-print '********************************************'
-#Brute data
-print 'Domain Name: ' + str(whoisData.domain_name)
-print 'Emails registered: ' + str(whoisData.emails)
-print 'Creation date: ' + str(whoisData.creation_date)
-print 'Expiration Date: ' + str(whoisData.expiration_date)
-print 'Last Update: ' + str(whoisData.updated_date)
-
+print '*****************************************************'
+print '*  Whois information'+ ' ' + str(data)+ ' ' + '*'
+print '*****************************************************'
+print 'Domain Name: ' + str(domain.domain_name)
+print 'Creation date: ' + str(domain.creation_date)
+print 'Expiration Date: ' + str(domain.expiration_date)
+print 'Register: ' + str(domain.registrar)
